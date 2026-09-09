196810e7e05d552ae48139f3aabeba7d2ae38cfa
Be My Code <gettofarmila@gmail.com>
2026-09-09 13:26:16 +0300

    format(ruff): Прогнал руфф форматирование

--- fpx/models/lots.py
+++ fpx/models/lots.py
@@ -15,33 +15,25 @@ class CurrentLotInfo:
     async def edit_price(self, new_price: float) -> Any:
         """Изменяет цену лота"""
         if not self._client:
-            raise fpx_err.FpxCriticalRunnerError(
-                "Объект CurrentLotInfo не привязан к клиенту fpx"
-            )
+            raise fpx_err.FpxCriticalRunnerError("Объект CurrentLotInfo не привязан к клиенту fpx")
         return await self._client.editor.change_lot_price(self.id, new_price)
 
     async def raise_lots(self) -> Any:
         """Поднимает все лоты"""
         if not self._client:
-            raise fpx_err.FpxCriticalRunnerError(
-                "Объект CurrentLotInfo не привязан к клиенту fpx"
-            )
+            raise fpx_err.FpxCriticalRunnerError("Объект CurrentLotInfo не привязан к клиенту fpx")
         return await self._client.lot.raise_lots()
 
     async def deactivate(self) -> Any:
         """Выключает лот"""
         if not self._client:
-            raise fpx_err.FpxCriticalRunnerError(
-                "Объект CurrentLotInfo не привязан к клиенту fpx"
-            )
+            raise fpx_err.FpxCriticalRunnerError("Объект CurrentLotInfo не привязан к клиенту fpx")
         return await self._client.editor.toggle_off_lot(self.id)
 
     async def activate(self) -> Any:
         """Включает лот"""
         if not self._client:
-            raise fpx_err.FpxCriticalRunnerError(
-                "Объект CurrentLotInfo не привязан к клиенту fpx"
-            )
+            raise fpx_err.FpxCriticalRunnerError("Объект CurrentLotInfo не привязан к клиенту fpx")
         return await self._client.editor.toggle_on_lot(self.id)
 
 
